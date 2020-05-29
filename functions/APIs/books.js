ea407ab0aad8076a3e817f9b3de93e9cefad7a26
ARAVINDBALAN <aravindbalan222@gmail.com>
2020-05-30 00:17:21 +0530

    update and delete

--- functions/APIs/books.js
+++ functions/APIs/books.js
@@ -4,7 +4,7 @@ const { db } = require('../utils/admin')
 
 
 exports.getAllBooks = async (req,res) => {
-    let books =  await db.collection('books').get()
+    let books =  await db.collection('books').orderBy('title','desc').get()
     let bookList =[]
     books.forEach(data => {
         bookList.push({
@@ -22,12 +22,43 @@ exports.addBooks = async (req,res) =>{
     
     const newBook = {
         title : req.body.title,
-        ISBN : req.body.ISBN,
+        isbn : req.body.isbn,
         author : req.body.author,
-        price : req.body.price
+        price : req.body.price,
+        category : req.body.category
     }
     let dbRef = db.collection('books')
     let newBookReturn = await dbRef.add(newBook)
     return res.json(newBookReturn.id)
+
+}
+
+
+exports.editBooks = async (req,res) =>{
+
+    const bookToUpdate = await db.doc('/books/${req.params.id}')
+    if(!bookToUpdate.exists){
+        return res.status(404).json({err : "No book found"})
+    }
+    else{
+        
+        await bookToUpdate.update(req.body)
+        return json(bookToUpdate.id)
+    }
+
+
+}
+
+
+exports.deleteBook = async (req,res) => {
+
+    const bookToDelete = await db.doc('/books/${req.params.id}')
+    if(!bookToDelete.exists){
+        return res.status(404).json({err : "No book found"})
+    }
+    else{
+        await bookToDelete.delete()
+        return res.json({status:"successful"})
+    }
 
 }
